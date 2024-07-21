c2b5ca8120ff028da74be40561a4cff99d1dbf46
Ubuntu <hugo_casqueiro_gmail_com@dev1-ubuntu2204.m1ux3tmyy2eu1hxsjumky3gekd.ax.internal.cloudapp.net>
2024-07-21 20:28:48 +0000

    wip: appwrite integration

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,21 @@
+'use server';
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database call / API call
+        
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Mutation / Database call / API call
+        // Create a user account
+        
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
